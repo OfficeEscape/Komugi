b88172640de8dd262e741fbfb132cfded3524023
王 秦如 <oshinjyo14@gmail.com>
2017-05-21 21:29:28 +0900

    ボタンのクリックアクションをGameObject生成時に自動で割り当てる機構を追加

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 
 public class GameMain : MonoBehaviour {
 
 	#region =============================== フィールド ===============================
-    /** 各画面のステージデータ */
-	private Dictionary<int, StageData> stageDictionary = new Dictionary<int, StageData> ();
-
     /** ゲームマネージャー */
     private GameManager gameManager;
+
+	/** 関数ディクショナリー */
+	Dictionary<string, UnityAction> functionDictionary = new Dictionary<string, UnityAction>();
 
     [SerializeField]
     /** メインキャンパス */
@@ -28,6 +29,7 @@ public class GameMain : MonoBehaviour {
 	{
         gameManager = GameManager.Instance;
 		gameManager.OpenBinary ();
+		InitButtonFunction ();
         //リソースフォルダのデータを非同期に読み込む
         StartCoroutine(LoadAsyncStageCoroutine(""));
 	}
@@ -40,45 +42,7 @@ public class GameMain : MonoBehaviour {
 
 	#endregion
 
-	#region =============================== C# private ===============================
-
-	// リソース非同期読み込み
-    private IEnumerator LoadAsyncStageCoroutine(string filePath)
-    {
-        // リソースの非同期読込開始
-        ResourceRequest resReq = Resources.LoadAsync(filePath);
-        // 終わるまで待つ
-        while (resReq.isDone == false)
-        {
-            Debug.Log("Loading progress:" + resReq.progress.ToString());
-            yield return 0;
-        }
-        // テクスチャ表示
-        Debug.Log("終了  " + Time.time.ToString());
-
-        //最初の画面を出す
-		ChangeView(gameManager.currentView);
-    }
-
-	private void ChangeView(int sceneId)
-	{
-		if (currentViewObject != null) { Destroy (currentViewObject);}
-		currentViewObject = Instantiate(Resources.Load("Prefabs/" + gameManager.stageDictionary[sceneId].prefab, typeof(GameObject))) as GameObject;
-		currentViewObject.transform.SetParent(mainCanvas.transform);
-		currentViewObject.transform.localPosition = Vector3.zero;
-		currentViewObject.transform.localScale = Vector3.one;
-		currentViewObject.transform.SetAsFirstSibling();
-
-		Button[] buttons = currentViewObject.GetComponentsInChildren<Button> ();
-		foreach (Button btn in buttons) 
-		{
-			btn.onClick.AddListener (JumpView);
-		}
-	}
-
-	#endregion
-
-	#region =============================== C# private ===============================
+	#region =============================== C# public ===============================
 
 	// 次のステージへ
 	public void nextView(int next)
@@ -115,6 +79,67 @@ public class GameMain : MonoBehaviour {
     {
 
     }
+
+	#endregion
+
+	#region =============================== C# private ===============================
+
+	// リソース非同期読み込み
+	private IEnumerator LoadAsyncStageCoroutine(string filePath)
+	{
+		// リソースの非同期読込開始
+		ResourceRequest resReq = Resources.LoadAsync(filePath);
+		// 終わるまで待つ
+		while (resReq.isDone == false)
+		{
+			Debug.Log("Loading progress:" + resReq.progress.ToString());
+			yield return 0;
+		}
+		// テクスチャ表示
+		Debug.Log("End  " + Time.time.ToString());
+
+		//最初の画面を出す
+		ChangeView(gameManager.currentView);
+	}
+
+	private void ChangeView(int sceneId)
+	{
+		if (currentViewObject != null) { Destroy (currentViewObject);}
+		currentViewObject = Instantiate(Resources.Load("Prefabs/" + gameManager.stageDictionary[sceneId].prefab, typeof(GameObject))) as GameObject;
+		if (currentViewObject == null) { Debug.Log ("Failed to Create View Object. ID : " + gameManager.currentView);}
+		currentViewObject.transform.SetParent(mainCanvas.transform);
+		currentViewObject.transform.SetAsFirstSibling();
+		currentViewObject.transform.localPosition = Vector3.zero;
+		currentViewObject.transform.localScale = Vector3.one;
+
+		Button[] buttons = currentViewObject.GetComponentsInChildren<Button> ();
+		for (int i = 0; i < buttons.Length; i++)
+		//foreach (Button btn in buttons)
+		{
+			SetButtonAction (buttons[i].tag, ref buttons[i]);
+			//btn.onClick.AddListener(JumpView);
+			Debug.Log (" set button action ");
+		}
+	}
+
+	// ボタンにクリックアクションを設定する
+	private void SetButtonAction(string tag, ref Button button)
+	{
+		if (!functionDictionary.ContainsKey (tag)) 
+		{
+			Debug.Log ("Tag Function : " + tag + " is Unregistered");
+			return;
+		}
+
+		button.onClick.AddListener (functionDictionary [tag]);
+	}
+
+	// なんの名前のボタンになんの処理を走らせる関数
+	// ちょっとやり方見苦しいが。。これ以上いい方法思いつかない。。
+	private void InitButtonFunction()
+	{
+		functionDictionary.Add ("Jump", JumpView);
+	}
 
 	#endregion
 }
