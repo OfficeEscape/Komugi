8286f827d88d1b1ce5f3e60eed71d5fb0d9961c9
王 秦如 <oshinjyo14@gmail.com>
2018-04-30 23:34:00 +0900

    広告調整

--- Assets/Scripts/ui/Hint.cs
+++ Assets/Scripts/ui/Hint.cs
@@ -12,8 +12,10 @@ namespace Komugi.UI
         [SerializeField]
         Button ReturnButton;
 
-	    // Use this for initialization
-	    void Start ()
+        private const string AD_KEY = "rewardedVideo";
+
+        // Use this for initialization
+        void Start ()
         {
             AdsButton.onClick.AddListener(() => { ShowRewardedAd(); });
             ReturnButton.onClick.AddListener(() => { Destroy(gameObject); });
@@ -22,7 +24,29 @@ namespace Komugi.UI
 	
 	    private void ShowRewardedAd()
         {
-            Advertisement.Show();
+            if (Advertisement.IsReady(AD_KEY))
+            {
+                var options = new ShowOptions { resultCallback = HandleShowResult };
+                Advertisement.Show(AD_KEY, options);
+            }
+            
+        }
+
+        private void HandleShowResult(ShowResult result)
+        {
+            switch (result)
+            {
+                case ShowResult.Finished:
+                    Debug.Log("アメ恵んでやろう.");
+                    
+                    break;
+                case ShowResult.Skipped:
+                    Debug.Log("スキップしやがったなコノヤロー.");
+                    break;
+                case ShowResult.Failed:
+                    Debug.LogError("失敗したみたい.");
+                    break;
+            }
         }
     }
 }
