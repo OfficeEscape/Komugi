9df3c0508dc51fbfc70214d14a0df6cd160849fb
濱崎史織 <melamine1217@gmail.com>
2018-05-27 19:49:59 +0900

    SEのcsファイル編集、ugui対応漏れ修正

--- Assets/Scripts/const/AudioConst.cs
+++ Assets/Scripts/const/AudioConst.cs
@@ -20,5 +20,34 @@
         public static readonly string SE_TEN_KEY_OPEN = "Openkey";
 
         public static readonly string SE_SWITCH_SE = "running_in_a_house";
+
+        //扉を開錠していない状態でドアをタップした時の音(共通)
+        public static readonly string SE_DOA_NOT_OPEN = "locker-open1";
+        //シンク下の扉とシューズケースの開閉音
+        public static readonly string SE_DOA_SINK_OPEN = "door-wood-knock1";
+        //ガラスドアの開閉音
+        public static readonly string SE_DOA_GLASS_OPEN = "door-close1";
+        //トイレのドアの開閉音
+        public static readonly string SE_DOA_TOILET_OPEN = "door-close2";
+        //バケツに水をためる音
+        public static readonly string SE_BUCKET_WATER = "tap3";
+        //雑巾を絞る音
+        public static readonly string SE_ZOUKIN_SHIBORU = "dustcloth-squeeze1";
+        //オットマンの汚れをふき取る音
+        public static readonly string SE_WIPE_OTTOMAN = "towel-wipe-body1";
+        //ガラスドア前のセンサーをクリアした音
+        public static readonly string SE_SENSOR_CLEAR = "decision3";
+        //パンフレットの紙をめくる音、暗号のカードを手に入れた音
+        public static readonly string SE_PAPER_FLICK = "card-turn-over1";
+        //電話機の暗号を解いた時に箱の開く音(シンク下の扉とシューズケースの開閉音と同じ)
+        public static readonly string SE_TELL_CLEAR = "door-wood-knock1";
+        //ロゴパーツを全てはめたときにエレベーターのパネルが起動する音
+        public static readonly string SE_ELEVATOR_PANEL = "button30";
+        //エレベーターの暗号入力時の音
+        public static readonly string SE_ELEVATOR_INPUT = "cursor1";
+        //エレベーターの暗号不正解
+        public static readonly string SE_ELEVATOR_FAIL = "faild";
+        //エレベーターの暗号正解！クリア
+        public static readonly string SE_ELEVATOR_SUCCESS = "success";
     }
 }
